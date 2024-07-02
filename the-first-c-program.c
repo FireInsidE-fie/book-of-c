dd0df463dc8b2a20ad9d6d626e1e11cadcf68607
Aisling Fontaine <aisling.fontaine@protonmail.com>
2024-07-02 14:43:32 +0200

    more experiementation on the hewwo world program

--- the-first-c-program.c
+++ the-first-c-program.c
@@ -2,5 +2,7 @@
 
 main()
 {
-  printf("hewwo, world\n");
+  printf("hewwo, world\n"); // So \n is for new line character
+  printf("okay but does this work if \aI add a back\bspace\n");
+  // I don't know what \a does but \b is a backspace
 }
