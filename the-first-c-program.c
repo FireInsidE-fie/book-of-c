20f3bfec0d0257e4a38bd3cac930c53dd71d5eeb
Aisling Fontaine <aisling.fontaine@protonmail.com>
2024-07-03 15:54:42 +0200

    add useless comments to first c program

--- the-first-c-program.c
+++ the-first-c-program.c
@@ -3,6 +3,8 @@
 main()
 {
   printf("hewwo, world\n"); // So \n is for new line character
-  printf("okay but does this work if \aI add a back\bspace\n");
+  printf("okay but does this work if \aI add \ta back\bspace\n");
   // I don't know what \a does but \b is a backspace
+  // apparently \a is for "alert"?? what does this mean??
+  // look at this https://en.wikipedia.org/wiki/Escape_sequences_in_C
 }
