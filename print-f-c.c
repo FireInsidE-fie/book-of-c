9d96e3ce0d606358c6b16372259829ff642ad200
Aisling Fontaine <aisling.fontaine@protonmail.com>
2024-07-04 15:27:14 +0200

    tabs are 4 spaces now. yay.

--- print-f-c.c
+++ print-f-c.c
@@ -1,22 +1,22 @@
 #include <stdio.h>
 
 /* print Fahrenheit-Celsius table
-    for fahr = 0, 20, ..., 300 */
+	for fahr = 0, 20, ..., 300 */
 
 main() {
-  int fahr, celsius;
-  int lower, upper, step;
-  
-  lower = 0;      /* lower limit of temperature scale */
-  upper = 300;    /* upper limit */
-  step = 20;      /* step size */
+	int fahr, celsius;
+	int lower, upper, step;
 
-  fahr = lower;
-  while (fahr <= upper)
-  {
-    celsius = 5 * (fahr-32) / 9;
-    printf("%d\t%d\t+ %d\n", fahr, celsius, step);
-    /* formating here is just "variable tab variable newline" */
-    fahr = fahr + step;
-  } 
+	lower = 0;      /* lower limit of temperature scale */
+	upper = 300;    /* upper limit */
+	step = 20;      /* step size */
+
+	fahr = lower;
+	while (fahr <= upper)
+	{
+		celsius = 5 * (fahr-32) / 9;
+		printf("%d\t%d\t + %d\n", fahr, celsius, step);
+		/* formating here is just "variable tab variable newline" */
+		fahr = fahr + step;
+	}  
 }
