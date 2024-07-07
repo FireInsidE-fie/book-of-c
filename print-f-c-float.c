f6e1ceba683b2f1f3dfb3c9ad8d5bc6d60d7d5f0
Aisling Fontaine <aisling.fontaine@protonmail.com>
2024-07-07 16:17:55 +0200

    add a header title to the output

--- print-f-c-float.c
+++ print-f-c-float.c
@@ -6,12 +6,15 @@
 main() {
     float fahr, celsius;
     float lower, upper, step;
+    char heading[] = "Table of conversions from fahrenheit to celsius";
 
     lower = 0;      // lower limit of temperature scale
     upper = 300;    // upper limit
     step = 20;      // step size
 
     fahr = lower;
+
+    printf("%s\n", heading);
     while (fahr <= upper)
     {
         celsius = (5.0/9.0) * (fahr-32.0);
