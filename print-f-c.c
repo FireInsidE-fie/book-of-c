aef2d7e2fcd85333d077407edf64704f73cff548
Aisling Fontaine <aisling.fontaine@protonmail.com>
2024-07-07 14:50:55 +0200

    try to follow conventions for comments

--- print-f-c.c
+++ print-f-c.c
@@ -7,16 +7,21 @@ main() {
 	int fahr, celsius;
 	int lower, upper, step;
 
-	lower = 0;      /* lower limit of temperature scale */
-	upper = 300;    /* upper limit */
-	step = 20;      /* step size */
+	lower = 0;      // lower limit of temperature scale
+	upper = 300;    // upper limit
+	step = 20;      // step size
 
 	fahr = lower;
 	while (fahr <= upper)
 	{
-		celsius = 5 * (fahr - 32) / 9; /* Converts here */
+		celsius = 5 * (fahr - 32) / 9; 
+		/* 
+		* Conversion here ; the division is done at the end because 
+		* otherwise the int type would truncate the value, making 5/9 become 0 
+		*/
+
 		printf("%d\t%d\t + %d\n", fahr, celsius, step);
-		/* formating here is just "variable tab variable + variable newline" */
+		// formating here is just "variable tab variable + variable newline"
 		fahr = fahr + step;
 	}
 }
