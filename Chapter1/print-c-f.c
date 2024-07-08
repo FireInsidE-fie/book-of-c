81d012d66c890768c0dcb2184acb744043b00213
Aisling Fontaine <aisling.fontaine@protonmail.com>
2024-07-08 14:39:36 +0200

    write program body, it works but also doesn't (values are wrong)

--- Chapter1/print-c-f.c
+++ Chapter1/print-c-f.c
@@ -1,7 +1,23 @@
 #include <stdio.h>
 
+/* Exercise 1-4 of the book of C */
 /* Present a table for converting celsius values to fahrenheit equivalents */
 
 main() {
-    
+    float lower, upper, step;
+    float celsius, fahrenheit;
+
+    lower = -20.0;
+    upper = 120.0;
+    step = 20.0;
+
+    celsius = lower;
+
+    while (celsius <= upper)
+    {
+        fahrenheit = celsius * 9 + 32;
+        printf("%3.0f\t%3.2f\t+ %2.0f\n", celsius, fahrenheit, step);
+
+        celsius = celsius + step;
+    }
 }
