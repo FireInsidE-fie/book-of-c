4c39b2f4cc40224cf60ffbb37d9a61e2b427df45
Aisling Fontaine <aisling.fontaine@protonmail.com>
2024-07-11 09:03:36 +0200

    begin writing f to c but with a for loop

---
+++ Chapter1/exercises/print-f-c-for.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/* Exercise 1-5 of the book of C */
+/* Print the conversion table from Fahrenheit to Celsius but with a for loop */
+
+main() {
+    int fahr;
+    
+    for (fahr = 0; fahr < 200; fahr = fahr + 20)
+    {
+        printf("", fahr, )
+    }
+}
