63a742dcd5ef0a8b33504149cb913396862618c9
Aisling Fontaine <aisling.fontaine@protonmail.com>
2024-07-12 14:42:32 +0200

    refactor cat.c and make it shorter

--- Chapter1/cat.c
+++ Chapter1/cat.c
@@ -2,20 +2,14 @@
 
 /* A simpler version of the unix `cat` command. This will print
     the contents of a file one character at a time if you redirect
-    its contents to the program via the command line */
+    its contents to the program via the command line
+    Example usage : `./cat.o < file.txt` */
 
 int main() {
     int c;
 
-    while(1) {
-        c = getchar();
-
+    while ((c = getchar()) != EOF)
         putchar(c);
-
-        if (c == EOF) {
-            return 0;
-        }
-    }
 
     return 0;
 }
