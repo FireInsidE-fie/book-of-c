c8c48a4a5004ceff582bda97934e8bbfdbc411c8
Aisling Fontaine <aisling.fontaine@protonmail.com>
2024-07-12 12:06:35 +0200

    added a little test for the EOF value

--- Chapter1/exercises/what-is-EOF.c
+++ Chapter1/exercises/what-is-EOF.c
@@ -6,6 +6,17 @@
     that getchar() != EOF is 0 or 1 */
 
 int main() {
+    // I know this is horrendous practice but it's just a test
+    while (1) {
+        int c = getchar();
+
+        if (c == EOF) {
+            return 0;
+        }
+
+        putchar(c);
+    }
+
     printf("%d\n", (getchar() != EOF));
 
     printf("EOF's value is: %d\n", EOF);
