4709e1d3c57603667deafc9a670eaa7d08f6e76d
Aisling Fontaine <aisling.fontaine@protonmail.com>
2024-07-13 15:39:34 +0200

    make it work but needs refactoring

--- Chapter1/exercises/replace-blanks.c
+++ Chapter1/exercises/replace-blanks.c
@@ -1,17 +1,22 @@
 #include <stdio.h>
 
 /* Exercice 1-9 page 22 of the book of c 
-    Prints std input to the output but multiple blanks by a single blank */
+    Prints std input to the output but replaces multiple blanks by a single blank */
 int main()
 {
     int c, last_c;
 
     while ((c = getchar()) != EOF)
     {
-        // If the last character and a current one are both a blank, do not send this one to output 
-        if (c != ' ')
-            putchar(c);    
+        // If the last character was a blank but this one isn't, send a blank
+        if (last_c == ' ' && c != ' ')
+            printf(" ");
 
+        if (c != ' ')
+        {
+            putchar(c);            
+        }
+        
         last_c = c; // Stores the last character sent to output
     }
     
