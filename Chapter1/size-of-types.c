b3628bbea65d0e584344ca77af5929de044eb7ff
Aisling Fontaine <aisling.fontaine@protonmail.com>
2024-07-13 11:44:13 +0200

    add the double type to the types size comparison

--- Chapter1/size-of-types.c
+++ Chapter1/size-of-types.c
@@ -4,6 +4,7 @@
 int main() {
     printf("Size of an int on this system : %zu bytes\n", sizeof(int));
     printf("Size of an long on this system : %zu bytes\n", sizeof(long));
+    printf("Size of a double on this system : %zu bytes\n", sizeof(double));
     printf("Int range : %d to %d\n", INT_MIN, INT_MAX);
     printf("Long range : %d to %d\n", LONG_MIN, LONG_MAX);
 
