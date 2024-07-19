e691dd3d67a52d156655418384bca6b9c6668eb0
Aisling Fontaine <aisling.fontaine@protonmail.com>
2024-07-19 14:15:06 +0200

    begin refactoring to print the histogram

--- Chapter1/exercises/word-length-histogram.c
+++ Chapter1/exercises/word-length-histogram.c
@@ -2,17 +2,20 @@
 
 #define IN 1  // state inside a word
 #define OUT 2  // state outside a word
+#define MAX_WORDS 10  // max words in input
+#define MAX_LENGTH 50  // max word length in input
 
 /* Exercise 1-13 page 25 of the book of C
     Draw a histogram of the length of the words inputted */
 int main()
 {
     int c = 'a', n_words = 0, state = 0;
+    char words[MAX_WORDS][MAX_LENGTH];  // words array
 
     /* count words */
     while ((c = getchar()) != EOF)
     {
-        // putchar(c);
+        // putchar(c);  // Debug
 
         if (c == ' ' || c == '\t' || c == '\n')
         {
@@ -27,12 +30,9 @@ int main()
 
     printf("Number of words: %d\n", n_words);
 
-    /* save the words in an array */
-
-    char words[n_words];
-
     /* print histogram */
-    for (int i = 0; i <= n_words ; ++i) {
+    printf("Histogram of word length:\n");
+    for (int i = 0; i <= MAX_WORDS ; ++i) {
         printf("%s\n", words[i]);
     }
 
