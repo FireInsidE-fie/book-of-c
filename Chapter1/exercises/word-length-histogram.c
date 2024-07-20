68ba59f573851fc4250c13d5b8bd20d3c98261c9
Aisling Fontaine <aisling.fontaine@protonmail.com>
2024-07-20 20:11:10 +0200

    add code to get length n print histogram

--- Chapter1/exercises/word-length-histogram.c
+++ Chapter1/exercises/word-length-histogram.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 #define IN 1  // state inside a word
 #define OUT 2  // state outside a word
@@ -12,24 +13,58 @@
 	TO DRAW THE HISTOGRAM LATER (AND TAKES LESS MEMORY PRESUMABLY) */
 int main()
 {
-	/* Get the words from the input - TO BE WRITTEN */
-	/* Placeholder */
+	// Operators for the while loops
+	int i = 0;
+	int j = 0;
 
+	/* Get the words from the input - TO BE WRITTEN */
+
+	/* Placeholder while we wait for word inputting code and direct
+		translation to word lengths */
 	char words[MAX_WORDS][MAX_LENGTH] = {"TEST1", "TEST2", "TEST3", "TEST4", 
 	"TEST5", "TEST6", "TEST7", "TEST8", "TEST9", "TEST10"};
 
-	/* Get the length for all the words - TO BE WRITTEN */
+	int lengths[MAX_WORDS];
 
-
-	/* Print out the histogram - TO BE WRITTEN */
-
-	int i = 0;
+	/* debug - print out the words array */
+	i = 0;
 	while (i < MAX_WORDS)
 	{
 		printf("%s\n", words[i]);  // debug
 		++i;
 	}
+
+	/* Get the length for all the words */
+
+	i = 0;
+	while (i < MAX_WORDS)
+	{
+		j = 0;
+		while (words[i][j] != '\0')
+		{
+			++j;
+		}
+
+		lengths[i] = j;
+		++i;
+	}
 	
+	/* Print out the histogram - TO BE WRITTEN */
+
+	i = 0;
+	while (i < MAX_WORDS)
+	{
+		j = 0;
+		while (j <= lengths[i])
+		{
+			putchar('-');
+			++j;
+		}
+
+		// printf("%d\n", lengths[i]);  // debug
+		printf("\n");  // Print a new line after a histogram bar is printed
+		++i;
+	}
 
 	return 0;
 }
