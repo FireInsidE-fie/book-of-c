341535f03cf37f7c46c15c7580f462915e599512
Aisling Fontaine <aisling.fontaine@protonmail.com>
2024-07-26 21:36:29 +0200

    add second version of power function

--- Chapter1/references/power.c
+++ Chapter1/references/power.c
@@ -22,3 +22,13 @@ int power(int base, int n)
         p = p * base;
     return p;
 }
+
+/* power: raise base to n-th power; n >= 0; version 2 */
+int power(int base, int n)
+{
+    int p;
+
+    for (p = 1; n > 0; --n)
+        p = p * base;
+    return p;
+}
