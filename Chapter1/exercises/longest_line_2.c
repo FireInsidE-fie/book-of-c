3290883d3fdb139ecbf2eb0ece065806081db41a
Aisling Fontaine <aisling.fontaine@protonmail.com>
2024-08-05 13:09:56 +0200

    get_line function works

--- Chapter1/exercises/longest_line_2.c
+++ Chapter1/exercises/longest_line_2.c
@@ -1,20 +1,25 @@
 #include <stdio.h>
 
+#define MAX_LENGTH 1000  // Max line length that the program will process
+
 /* Exercise page 31 of the book of C
 	Print the longest line from all the input
 	Technically I just have to add something to another program but I thought
 	it would be fun to rewrite the whole thing (I need to lock in) */
 
-void get_line()
+// Return the length of the current line of input
+int get_line()
 {
 	int c;  // While loop operator
+	int length;  // The length of the current line that will be returned
 
 	c = 0;
+	length = 0;
 	while ((c = getchar()) != '\n')
 	{
-		printf("%d", c);
+		++length;
 	}
-	
+	return length;
 }
 
 void main()
@@ -22,5 +27,7 @@ void main()
 	int current_length, max_length;
 	int i, j;  // While operators
 
+	current_length = get_line();
+	printf("The line was %d characters long.\n", current_length);
 }
 
